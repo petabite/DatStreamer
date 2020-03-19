5d41df931272d00211b34d57e1a77f717bbfc59c
petabite <jmslca123@gmail.com>
2020-03-18 21:35:56 -0700

    bug fixes + styling

--- src/Track.java
+++ src/Track.java
@@ -69,7 +69,8 @@ public class Track implements Serializable {
 
     private String convertTrackName() {
         String num = (track_num < 10) ? "0" + Integer.toString(track_num) : Integer.toString(track_num);
-        String tmp_title = title.replaceAll("['|.|#|,|&]", "")
+        String tmp_title = title.substring(0, (title.length() <= 50) ? title.length() : 50).trim()
+                .replaceAll("['|.|#|,|&|\\-|$|:]", "")
                 .replaceAll("\\[", "%5B")
                 .replaceAll("]", "%5D")
                 .replaceAll("\\s\\)", "");
